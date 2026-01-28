97f532bf0d1f876adae2e10576cbc0a424b4af5f
AliAngel564 <jonathan.aliangel@gmail.com>
2026-01-28 01:28:25 -0600

    🚧
    E prompt aparece y mira hacia el jugador

---
+++ Assets/Scripts/DialoguePrompt.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+
+public class DialoguePrompt : MonoBehaviour
+{
+    private SphereCollider dialogueCollider;
+    private Transform npcTransform;
+    [Header("Prompt Spot Transform")]
+    [SerializeField] private Transform promptSpot;
+
+    private Quaternion currentPromtRotation;
+    
+    [Header("Prompt Canvas")]
+    [SerializeField]private Canvas promptCanvas;
+    
+    private void Start()
+    {
+        npcTransform = GetComponentInParent<Transform>();
+        promptSpot.position = new Vector3(npcTransform.position.x, npcTransform.position.y+2f, npcTransform.position.z);
+        dialogueCollider = GetComponent<SphereCollider>();
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            promptCanvas.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            promptCanvas.enabled = false;
+        }
+    }
+}
